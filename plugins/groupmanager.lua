473315f1b5a6e38c0eb692f15b4767e62962a268
ArmanIr <memfi4@yahoo.com>
2015-10-25 00:10:52 +0330

    Update groupmanager.lua

--- plugins/groupmanager.lua
+++ plugins/groupmanager.lua
@@ -5,9 +5,7 @@ do
 local function create_group(msg)
     -- superuser and admins only (because sudo are always has privilege)
     if not is_admin(msg) then
-        return "Sorry You Don't Have Access
-        ببخشید شما مجاز به استفاده از این بخش نمیباشید
-        سازنده: @thisisarman"
+        return "Sorry You Don't Have Access contact: @thisisarman"
     end
     local group_creator = msg.from.print_name
     create_group_chat (group_creator, group_name, ok_cb, false)
