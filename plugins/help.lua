450fb5ccea2f512bd85a3d85549d945e2d059d6e
ArmanIr <memfi4@yahoo.com>
2015-10-25 16:08:05 +0330

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -110,8 +110,7 @@ local function telegram_help()
   end
   text = text..'\n'..'There are '..i..' plugins help available.'
   text = text..'\n'..'Write "!help [plugin name]" or "!help [plugin number]" for more info.'
-  text = text..'\n'..'Or "!help all" to show all info.
-  BOT BY : @ThisIsArman'
+  text = text..'\n'..'Or "!help all" to show all info.'
   return text
 end
  
